ff2265529b7c86404d8883b4a3698cf76f49e4ef
Shardul Patil <patilshardulsatish@yahoo.com>
2020-12-10 18:52:32 +0530

    Refactor : Added code To compute a+b*c

--- sortArithematicComputation.sh
+++ sortArithematicComputation.sh
@@ -5,3 +5,5 @@ read -p "Enter First Number : " a
 read -p "Enter Second Number : " b
 read -p "Enter a Third Number : " c
 
+value=`echo | awk -v a=$a -v b=$b -v c=$c '{compute=a+b*c} END {print compute}'`
+
