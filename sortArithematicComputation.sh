0137127c8bb4436b88dc78d17e8a62da3875ff0c
Shardul Patil <patilshardulsatish@yahoo.com>
2020-12-10 19:15:13 +0530

    Refactor : refactored code to Store computed values in a dictionary

--- sortArithematicComputation.sh
+++ sortArithematicComputation.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/bash -x
 echo " welcome to compute and sort arithematic numbers "
 
+declare -A computeVals
+
 read -p "Enter First Number : " a
 read -p "Enter Second Number : " b
 read -p "Enter a Third Number : " c
 
-val1=`echo | awk -v a=$a -v b=$b -v c=$c '{compute=a+b*c} END {print compute}'`
-val2=`echo | awk -v a=$a -v b=$b -v c=$c '{compute=a*b+c} END {print compute}'`
-val3=`echo | awk -v a=$a -v b=$b -v c=$c '{compute=c+a/b} END {print compute}'`
-val4=`echo | awk -v a=$a -v b=$b -v c=$c '{compute=a%b+c} END {print compute}'`
+computeVals[1]=`echo | awk -v a=$a -v b=$b -v c=$c '{compute=a+b*c} END {print compute}'`
+computeVals[2]=`echo | awk -v a=$a -v b=$b -v c=$c '{compute=a*b+c} END {print compute}'`
+computeVals[3]=`echo | awk -v a=$a -v b=$b -v c=$c '{compute=c+a/b} END {print compute}'`
+computeVals[4]=`echo | awk -v a=$a -v b=$b -v c=$c '{compute=a%b+c} END {print compute}'`
 
