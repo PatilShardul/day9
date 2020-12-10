0950535b80bfba745fb54bba68661ef7c0d11717
Shardul Patil <patilshardulsatish@yahoo.com>
2020-12-10 18:46:27 +0530

    Refactor : Added a Code to take 3 input from user

--- sortArithematicComputation.sh
+++ sortArithematicComputation.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/bash -x
 echo " welcome to compute and sort arithematic numbers "
+
+read -p "Enter First Number : " a
+read -p "Enter Second Number : " b
+read -p "Enter a Third Number : " c
+
